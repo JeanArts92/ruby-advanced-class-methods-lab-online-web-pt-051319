a11b85f9f148231d830606f09c3103a6980bc82d
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-28 15:14:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,12 @@ class Song
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end 
+  
+  def self.alphabetical 
+    self.all.collect{|s| s.name.sort}
+  end 
+    
+  end 
     
   
 end 

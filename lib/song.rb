dacb75abd1260e3eaec2c99ca948e039e5dfc43c
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-28 15:42:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,16 @@ class Song
     self.all.sort_by{|s| s.name}
   end 
   
-  
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
     
   
    
